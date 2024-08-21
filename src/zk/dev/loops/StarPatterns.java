7d9b629c3af93d638c459f47cd74dc9f78b352d3
Zeeshan Khan <zeeshank1@gmail.com>
2024-08-22 01:14:02 +0530

    Star Pattern using nested loop

---
+++ src/zk/dev/loops/StarPatterns.java
@@ -0,0 +1,49 @@
+package zk.dev.loops;
+
+public class StarPatterns {
+
+    public static void main(String[] args) {
+
+        //Simple Pattern-1
+        for ( int i = 1; i <= 3; i++ ) {
+
+            for ( int j = 1; j < i + 1; j++ ) {
+
+                System.out.print("* ");
+
+            }
+            System.out.println( );
+        }
+
+        System.out.println("------------------------------");
+
+        //Reverse Star Pattern-2
+        for ( int i = 1; i <= 3; i++ ) {
+
+            for ( int j = 3; j > i - 1; j-- ) {
+
+                System.out.print("* ");
+
+            }
+            System.out.println( );
+        }
+
+        System.out.println("------------------------------");
+
+        //Special Star Pattern-3
+        for ( int i = 1; i <= 3; i++ ) {
+
+            for ( int j = 3; j > i - 1; j-- ) {
+                System.out.print(" ");
+            }
+
+            for ( int k = 0; k < i; k++ ) {
+                System.out.print("* ");
+            }
+
+            System.out.println( );
+        }
+
+
+    }
+}
