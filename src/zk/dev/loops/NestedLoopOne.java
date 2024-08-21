824b1d410f06c857024bb2e896570324276006d1
Zeeshan Khan <zeeshank1@gmail.com>
2024-08-21 23:21:07 +0530

    Nested Loops changes and code formatting

--- src/zk/dev/loops/NestedLoopOne.java
+++ src/zk/dev/loops/NestedLoopOne.java
@@ -5,25 +5,39 @@ public class NestedLoopOne {
     public static void main(String[] args) {
 
         // Loop-1
-        for (int i = 0; i < 3; i++) {
+        for ( int i = 0; i < 3; i++ ) {
 
-            for (int j = 0; j < 3; j++) {
+            for ( int j = 0; j < 3; j++ ) {
 
-                System.out.println(i+" "+j);
+                System.out.println(i + " " + j);
             }
-            System.out.println();
+            System.out.println( );
         }
 
+        System.out.println("------------------------------");
         // Loop-2
+        System.out.println("     j  j  j ");
+        for ( int i = 0; i < 3; i++ ) {
+            System.out.print( "i  [");
+            for ( int j = 0; j < 3; j++ ) {
+
+                System.out.print(" " + j+" ");
+            }
+            System.out.print("] ");
+            System.out.println( );
+        }
+
+        System.out.println("------------------------------");
+        // Loop-3
         // Please Note : Due to variable scope of int i and int j,
         // There is no problem int i declaration again since it is in inside for loop.
-        for (int i = 0; i < 3; i++) {
+        for ( int i = 0; i < 3; i++ ) {
 
-            for (int j = 2; j >= 0; j--) {
+            for ( int j = 2; j >= 0; j-- ) {
 
-                System.out.println(i+" "+j);
+                System.out.println(i + " " + j);
             }
-            System.out.println();
+            System.out.println( );
         }
     }
 }
