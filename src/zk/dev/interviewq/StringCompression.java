3ff610557bb3aae41a963afbdab7a73a5ee958e2
Zeeshan Khan <zeeshank1@gmail.com>
2024-09-07 20:03:30 +0530

    Compress the String by counting the repetitive chars

---
+++ src/zk/dev/interviewq/StringCompression.java
@@ -0,0 +1,36 @@
+package zk.dev.interviewq;
+
+//AAABBCDDAA--> A3B2CD2A2
+public class StringCompression {
+    public static String stringCompress(String inputStr) {
+        StringBuilder sb = new StringBuilder( );
+        char[] inputStrArr = inputStr.toCharArray( );
+
+        int loopStarter = 0;
+        for ( int i = loopStarter; i < inputStrArr.length; i++ ) {
+
+            char currentChar = inputStrArr[loopStarter];
+            int count = 0;
+
+            for ( int k = loopStarter; k < inputStrArr.length; k++ ) {
+                if (currentChar == inputStrArr[k]) {
+                    count++;
+                    if (k == inputStrArr.length - 1) {
+                        sb.append(currentChar).append(count);
+                        return sb.toString( );
+                    }
+                } else {
+                    sb.append(currentChar).append(count);
+                    loopStarter = count + loopStarter;
+                    break;
+                }
+            }
+        }
+
+        return sb.toString( );
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringCompress("ABAA"));
+    }
+}
